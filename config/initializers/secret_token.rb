f790f6f4f1be61b6a8eb0833b3efde96285ef9fc
Kenta Murata <mrkn@mrkn.jp>
2011-09-12 23:05:01 +0900

    start project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TddbcRuby::Application.config.secret_token = '90f4df4c645ce33ed4f273d0a9b6a8edf02aa92f12dd48d2bf772cfd478bd60261395234215900aecd04c724f700f36b8422b23ebc5f7fe76670da810a047c14'
